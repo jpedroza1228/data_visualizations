ea39ca20d89b9e650e327e55ad2f7751fd54a1b5
Jonathan Pedroza (JP) <jpedroza@uoregon.edu>
2020-03-03 14:58:03 -0800

    west coast plot

--- corona_virus_data/corona_visualizations.R
+++ corona_virus_data/corona_visualizations.R
@@ -45,23 +45,68 @@ death_animate <- beer %>%
 
 animate(death_animate, fps = 10)
 
-us <- map_data(map = 'usa', subregion = 'county')
+us <- map_data(map = 'usa')
+
 
 little_beer <- beer %>% 
   filter(country_region == 'US')
+
 
 us_animate <- little_beer %>% 
   filter(province_state != 'Unassigned Location (From Diamond Princess)') %>% 
   ggplot() +
   geom_polygon(data = us, aes(x = long, y = lat, group = group), fill = NA, color = 'gray70', alpha = .3) +
-  geom_point(aes(x = long, y = lat, color = type)) +
+  geom_point(aes(x = long, y = lat, color = type), size = 2) +
   coord_fixed(1.3) +
   scale_color_manual(values = c('#1A8CFE', '#FE2F1A', '#8E49FF')) +
-  transition_time(date) +
-  ease_aes('linear') +
-  labs(title = 'Date: {frame_time}') +
-  theme_classic()
+  # transition_time(date) +
+  # ease_aes('linear') +
+  # labs(title = 'Date: {frame_time}') +
+  theme_nothing() +
+  theme(legend.position = 'none')
 
-animate(us_animate)
-anim_save('us_cases.gif')
+us_animate
 
+# animate(us_animate, fps = 100)
+# anim_save('us_cases.gif')
+
+
+county <- map_data('county')
+
+
+west_coast <- county %>% 
+  filter(region == 'california' |
+           region == 'oregon' |
+           region == 'washington')
+
+
+
+west_coast_beer <- little_beer %>% 
+  filter(province_state != 'Unassigned Location (From Diamond Princess)') %>% 
+  separate(province_state, 
+           into = c('county', 'state'),
+           sep = ',') %>% 
+  mutate(county = str_remove_all(county, "County")) %>% 
+  filter(state == ' CA' |
+           state == ' WA' |
+           state == ' OR')
+
+
+counts_by_state <- west_coast_beer %>% 
+  group_by(type, state) %>% 
+  summarize(total_state = sum(cases))
+
+counts_all <- west_coast_beer %>% 
+  group_by(type) %>% 
+  summarize(total_state = sum(cases))
+
+
+
+west_coast_beer %>% 
+  ggplot() +
+  geom_polygon(data = west_coast, aes(x = long, y = lat, group = group), fill = NA, color = 'gray70', alpha = .3) +
+  geom_point(aes(x = long, y = lat, color = type), size = 1.5) +
+  coord_fixed(1.3) +
+  scale_color_manual(values = c('#1A8CFE', '#FE2F1A', '#8E49FF'),
+                     labels = paste(counts_all$type, counts_all$total_state)) +
+  theme_classic() 
