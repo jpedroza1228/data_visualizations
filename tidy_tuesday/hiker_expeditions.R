095b434ef779ce58ddaa3522620a00c699844e4d
JP <jpedroza@uoregon.edu>
2021-12-12 18:18:32 -0800

    more files deleted

--- tidy_tuesday/hiker_expeditions.R
+++
@@ -1,9 +0,0 @@
-members <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/members.csv')
-expeditions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
-peaks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
-
-library(tidyverse)
-# install.packages('skimr')
-library(tidymodels)
-
-skimr::skim(members)
